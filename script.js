078202b865f50e29c72f82c124f0e2f1faa51211
espenraugstad <espen.raugstad@gmail.com>
2023-10-06 11:40:23 +0200

    Added a function to draw  boxes when numerator loses focus

--- script.js
+++ script.js
@@ -1,54 +1,63 @@
-const numerator = document.getElementById('numerator');
-const denominator = document.getElementById('denominator');
-const run = document.getElementById('run');
-const boxes = document.getElementById('boxes');
-const count = document.getElementById('count');
-const bigCount = document.getElementById('bigCount');
-const result = document.getElementById('result');
+const numerator = document.getElementById("numerator");
+const denominator = document.getElementById("denominator");
+const run = document.getElementById("run");
+const boxes = document.getElementById("boxes");
+const count = document.getElementById("count");
+const bigCount = document.getElementById("bigCount");
+const result = document.getElementById("result");
 
 run.addEventListener("click", runSimulation);
 
-async function runSimulation(){
-    console.log("Running");
-    // Clear existing boxes
-    boxes.innerHTML = "";
-    bigCount.innerHTML = "0";
-    result.innerHTML = "";
-    // Draw up the required number of boxes
-    for(let i = 0; i < parseInt(numerator.value); i++){
-        let box = document.createElement("span");
-        box.classList.add("box");
-        boxes.appendChild(box);
-    }
-    const allBoxes = document.querySelectorAll(".box");
-    await sleep(2000);
-    
-    // Hide the first boxes
-    const totalBoxes = parseInt(numerator.value);
-    const blocks = parseInt(denominator.value);
-    let currentCount = 0;
-    while((currentCount + 1) * blocks <= totalBoxes){
-    
-        for(let i = currentCount * blocks; i < currentCount * blocks + blocks; i++){
-            allBoxes[i].classList.add("hiddenBox");
-        }
-        currentCount++;
-        console.log(currentCount * blocks);
-        bigCount.innerHTML = currentCount;
-        await sleep(2000);
-    }
+numerator.addEventListener("blur", drawBoxes);
 
-    showResult();
+async function runSimulation() {
+  console.log("Running");
+  
+  const allBoxes = document.querySelectorAll(".box");
+  await sleep(2000);
+
+  // Hide the first boxes
+  const totalBoxes = parseInt(numerator.value);
+  const blocks = parseInt(denominator.value);
+  let currentCount = 0;
+  while ((currentCount + 1) * blocks <= totalBoxes) {
+    for (
+      let i = currentCount * blocks;
+      i < currentCount * blocks + blocks;
+      i++
+    ) {
+      allBoxes[i].classList.add("hiddenBox");
+    }
+    currentCount++;
+    console.log(currentCount * blocks);
+    bigCount.innerHTML = currentCount;
+    await sleep(2000);
+  }
+
+  showResult();
 }
 
-function showResult(){
-    // Get the number of boxes who don't have the class hiddenBox
-    let remainingBoxes = document.querySelectorAll(".box:not(.hiddenBox)");
-    console.log(remainingBoxes);
-    result.innerHTML = `${numerator.value} % ${denominator.value} = ${remainingBoxes.length}`;
+function drawBoxes() {
+  // Clear existing boxes
+  boxes.innerHTML = "";
+  bigCount.innerHTML = "0";
+  result.innerHTML = "";
+  // Draw up the required number of boxes
+  for (let i = 0; i < parseInt(numerator.value); i++) {
+    let box = document.createElement("span");
+    box.classList.add("box");
+    boxes.appendChild(box);
+  }
+}
+
+function showResult() {
+  // Get the number of boxes who don't have the class hiddenBox
+  let remainingBoxes = document.querySelectorAll(".box:not(.hiddenBox)");
+  console.log(remainingBoxes);
+  result.innerHTML = `${numerator.value} % ${denominator.value} = ${remainingBoxes.length}`;
 }
 
 /*** UTILITY FUNCTIONS ***/
-function sleep(ms){
-    return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
